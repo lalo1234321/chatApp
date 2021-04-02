68763bf7cce2b7679e492962a49489c12cd0fc00
lalo1234321 <edominguezc@toluca.tecnm.mx>
2021-04-01 20:56:59 -0600

    first configurations for the chat page

--- lib/pages/chat_page.dart
+++ lib/pages/chat_page.dart
@@ -5,11 +5,43 @@ class ChatPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Chat page'),
-      ),
-      body: Center(
-        child: Text('chat page'),
-      ),
+        backgroundColor: Colors.white,
+        title: Center(
+          child: Column(
+            children: <Widget>[
+              CircleAvatar(
+                child: Text('Te', style: TextStyle(fontSize: 12)),
+                backgroundColor: Colors.blue[100],
+                maxRadius: 15,
+              ),
+              Text('Test1',style: TextStyle(color: Colors.black, fontSize: 12),)
+            ],
+          ),
+        ),
+        centerTitle: true,
+        elevation: 1,
+      ),  //para hacer el cuerpo usaremos flexible para que se expanda según lo que tengamos
+      body: Column(
+        children: <Widget>[
+          Flexible(
+            // hay que recordar que el listView.builder se va generando con base en la demanda
+            child: ListView.builder(
+              itemBuilder: (BuildContext context, i) {
+                return Text('${i}');
+              },
+              reverse: true,
+              physics: BouncingScrollPhysics(),
+              )
+          ),
+          Divider( height: 1,),
+          //TODO  caja de texto
+          Container(
+            color: Colors.white,
+            height: 50,
+          )
+
+        ],
+      )
     );
   }
 }
